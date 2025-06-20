e8f7ce2b2f9f9f0453bb00ac4629d2a8e8a7f95d
apadros <--->
2025-06-20 16:13:36 +0200

    Added basic loading of calendar.txt and displaying of its contents

--- source/compile.bat
+++ source/compile.bat
@@ -1,5 +1,7 @@
 @echo off
 
+if %1.==. ( goto Help )
+
 if not exist build (
   mkdir build
 )
@@ -8,3 +10,9 @@ pushd build
 cl /I..\..\lib64 ..\%1 /c
 
 popd
+exit /b
+
+:Help
+echo:
+echo Format: compile.bat file.cpp
+echo:
--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,19 @@
 #include "apad_base_types.h"
+#include "apad_error.h"
+#include "apad_file.h"
 #include "apad_win32.h"
 
+#include <stdio.h>
 ConsoleAppEntryPoint(args, argsCount) {
+	const char* path = "data/calendar.txt";
+	bool b = FileExists(path);
+	bool error = ErrorIsSet();
+	
+	auto file = LoadFile(path);
+	error = ErrorIsSet();
+	char* test = (char*)file.memory;
+	
+	printf("\n%s\n", test);
 	
 	return 0;
 }
